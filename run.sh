ce4eb3f46cd6ea1c0225baf39437a513fe8a8110
Ubuntu <ubuntu@ip-172-31-28-184.ap-northeast-1.compute.internal>
2023-09-12 13:26:16 +0000

    aws

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 # https://github.com/iarai/NeurIPS2022-traffic4cast
 
 PATH=/usr/share/anaconda3/bin:.:$PATH
-#echo $PATH
-#conda env update -f environment.yaml
+echo $PATH
 
+#if true; then
 if false; then
+  #conda env update -f environment.yaml
   mkdir t4c22data
   cd t4c22data
   wget http://bigtmp.mathema-tech.com/data/MELBOURNE_2022.zip
@@ -15,9 +16,6 @@ if false; then
   unzip MELBOURNE_2022.zip
   unzip T4C_INPUTS_2022.zip
   unzip T4C_INPUTS_ETA_2022.zip
-  rm MELBOURNE_2022.zip
-  rm T4C_INPUTS_2022.zip
-  rm T4C_INPUTS_ETA_2022.zip
   rm MELBOURNE_2022.zip
   rm T4C_INPUTS_2022.zip
   rm T4C_INPUTS_ETA_2022.zip
@@ -41,35 +39,39 @@ unset __conda_setup
 # <<< conda initialize <<<
 
 #conda info --envs
-conda activate t42cc
+conda activate t4c22
 CUDA="cu115"
 #CUDA="cu113"
 
-#python -m pip install -r install-extras-torch-geometric.txt -f https://data.pyg.org/whl/torch-1.11.0+${CUDA}.html
-#python t4c22/misc/check_torch_geometric_setup.py
+if true; then
+#if false; then
+  python -m pip install -r install-extras-torch-geometric.txt -f https://data.pyg.org/whl/torch-1.11.0+${CUDA}.html
+  python t4c22/misc/check_torch_geometric_setup.py
 
-#if true; then
-if false; then
+  echo prepare1
   #export -p  | grep PYTHON
-  echo prepare
   export PYTHONPATH="."
   python t4c22/prepare_training_data_cc.py -d t4c22data > t4c22/tmp1.txt 2>&1
+  echo prepare2
   python t4c22/prepare_training_data_eta.py -d t4c22data > t4c22/tmp2.txt 2>&1
-  #python t4c22/prepare_training_check_labels.py -d ../t4c22/data/merged > t4c22/tmp3.txt 2>&1
+  echo prepare3
+  python t4c22/prepare_training_check_labels.py -d t4c22data > t4c22/tmp3.txt 2>&1
+  echo prepareend
 fi
 
 #cd data
 #jupyter execute data_preprocess.ipynb
 
 
-if false; then
-#if true; then
+#if false; then
+if true; then
   echo chkdata
   python -u tool/chkdata.py t4c22data/train/melbourne/cluster_input/counters_2020-06-02.parquet
+  echo chkdataend
 fi
 
-#if true; then
-if false; then
+if true; then
+#if false; then
   # screen run.sh
   echo cluster
   ip2p.sh model/cluster.ipynb > model/cluster.py
@@ -85,8 +87,8 @@ if true; then
   echo model
   cd model
   rm tmp1.txt
-  python -u GNN_model_train.py
-  #python -u GNN_model_train.py > tmp1.txt 2>&1
+  #python -u GNN_model_train.py
+  python -u GNN_model_train.py > tmp1.txt 2>&1
   #python -u GNN_model_test.py >> tmp1.txt 2>&1
   #python -u submission_cc.py >> tmp1.txt 2>&1 
   #python -u submission_eta.py >> tmp1.txt 2>&1 
