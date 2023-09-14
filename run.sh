dcfb49f533c1e46898b6c4047edca32fa415910d
Ubuntu <ubuntu@ip-172-31-12-210.ap-northeast-1.compute.internal>
2023-09-14 05:26:37 +0000

    aws

--- run.sh
+++ run.sh
@@ -122,7 +122,7 @@ if true; then
   echo model
   cd model
   rm tmp1.txt
-  #python -u GNN_model_train.py; exit
+  date; python -u GNN_model_train.py; date; exit
   echo train
   date
   python -u GNN_model_train.py > tmp1.txt 2>&1
