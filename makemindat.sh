9555d799b5dea8e8ff4c0c69772090af22534b23
Ubuntu <ubuntu@ip-172-31-12-210.ap-northeast-1.compute.internal>
2023-10-11 07:02:04 +0000

    makemin

---
+++ makemindat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOME=/ihdd/ubuntu
+ODATA=$HOME/t4c22data
+DATA=$HOME/mindata
+mkdir $DATA
+mkdir $DATA/road_graph
+mkdir $DATA/road_graph/melbourne
+
+cp $ODATA/road_graph/melbourne/road_graph_edges.parquet $DATA/road_graph/melbourne/road_graph_edges.parquet
+cp $ODATA/road_graph/melbourne/road_graph_nodes.parquet $DATA/road_graph/melbourne/road_graph_nodes.parquet
+
+mkdir $DATA/train
+mkdir $DATA/train/melbourne
+mkdir $DATA/train/melbourne/input
+cp $ODATA/train/melbourne/input/* $DATA/train/melbourne/input
